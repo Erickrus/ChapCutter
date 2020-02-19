41ca532105cae6072bfe781652e31274f0fd40eb
Erickrus <hyinghao@hotmail.com>
2020-02-19 21:09:14 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace ChapCutter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbxRegularExpression = new System.Windows.Forms.TextBox();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.tbxOutputFilename = new System.Windows.Forms.TextBox();
+            this.tbxOutputDirectory = new System.Windows.Forms.TextBox();
+            this.tbxFilename = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lvPreview = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxEveryNChapter = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(53, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "小说章节分割器 C#";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbxEveryNChapter);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbxRegularExpression);
+            this.groupBox1.Controls.Add(this.btnOutput);
+            this.groupBox1.Controls.Add(this.btnAnalyze);
+            this.groupBox1.Controls.Add(this.tbxOutputFilename);
+            this.groupBox1.Controls.Add(this.tbxOutputDirectory);
+            this.groupBox1.Controls.Add(this.tbxFilename);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(15, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 161);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "选项(&P)：";
+            // 
+            // tbxRegularExpression
+            // 
+            this.tbxRegularExpression.Location = new System.Drawing.Point(77, 104);
+            this.tbxRegularExpression.Name = "tbxRegularExpression";
+            this.tbxRegularExpression.Size = new System.Drawing.Size(228, 21);
+            this.tbxRegularExpression.TabIndex = 10;
+            this.tbxRegularExpression.Text = "第(.){1,10}章";
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Enabled = false;
+            this.btnOutput.Location = new System.Drawing.Point(267, 132);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(62, 23);
+            this.btnOutput.TabIndex = 12;
+            this.btnOutput.Text = "输出(&O)";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(194, 132);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(64, 23);
+            this.btnAnalyze.TabIndex = 11;
+            this.btnAnalyze.Text = "分析(&A)";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbxOutputFilename
+            // 
+            this.tbxOutputFilename.Location = new System.Drawing.Point(78, 78);
+            this.tbxOutputFilename.Name = "tbxOutputFilename";
+            this.tbxOutputFilename.Size = new System.Drawing.Size(114, 21);
+            this.tbxOutputFilename.TabIndex = 9;
+            // 
+            // tbxOutputDirectory
+            // 
+            this.tbxOutputDirectory.Location = new System.Drawing.Point(78, 51);
+            this.tbxOutputDirectory.Name = "tbxOutputDirectory";
+            this.tbxOutputDirectory.Size = new System.Drawing.Size(227, 21);
+            this.tbxOutputDirectory.TabIndex = 9;
+            // 
+            // tbxFilename
+            // 
+            this.tbxFilename.Location = new System.Drawing.Point(78, 21);
+            this.tbxFilename.Name = "tbxFilename";
+            this.tbxFilename.Size = new System.Drawing.Size(227, 21);
+            this.tbxFilename.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(6, 134);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "删除空行";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(19, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "|";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(19, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "|";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "查找条件：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "输出文件：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "输出路径：";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "选择文件：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lvPreview);
+            this.groupBox2.Location = new System.Drawing.Point(18, 232);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(334, 193);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "预览(&V)：";
+            // 
+            // lvPreview
+            // 
+            this.lvPreview.CheckBoxes = true;
+            this.lvPreview.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lvPreview.Location = new System.Drawing.Point(6, 20);
+            this.lvPreview.Name = "lvPreview";
+            this.lvPreview.Size = new System.Drawing.Size(320, 165);
+            this.lvPreview.TabIndex = 0;
+            this.lvPreview.UseCompatibleStateImageBehavior = false;
+            this.lvPreview.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "内容";
+            this.columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "行号";
+            this.columnHeader2.Width = 50;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(218, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 12);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Eric Hu";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblStatus);
+            this.groupBox3.Location = new System.Drawing.Point(18, 426);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(334, 34);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(6, 16);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(89, 12);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "请打开文件 ...";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "*.txt | *.txt";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::ChapCutter.Properties.Resources.icoChapCuttor;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(199, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "每个文件：";
+            // 
+            // tbxEveryNChapter
+            // 
+            this.tbxEveryNChapter.Location = new System.Drawing.Point(257, 77);
+            this.tbxEveryNChapter.Name = "tbxEveryNChapter";
+            this.tbxEveryNChapter.Size = new System.Drawing.Size(48, 21);
+            this.tbxEveryNChapter.TabIndex = 14;
+            this.tbxEveryNChapter.Text = "1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(326, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "章";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 476);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "章节分割器 (Chap Cutter) C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxOutputFilename;
+        private System.Windows.Forms.TextBox tbxOutputDirectory;
+        private System.Windows.Forms.TextBox tbxFilename;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.ListView lvPreview;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tbxRegularExpression;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tbxEveryNChapter;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ChapCutter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.tbxOutputDirectory.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.tbxFilename.Text = openFileDialog1.FileName;
+                String f = tbxFilename.Text.Substring(tbxFilename.Text.LastIndexOf(@"\") + 1);
+                String name = f.Substring(0, f.LastIndexOf("."));
+                String suffix = f.Substring(f.LastIndexOf("."));
+                this.tbxOutputFilename.Text = name + "{0}" + suffix;
+
+                this.tbxOutputDirectory.Text = tbxFilename.Text.Substring(0,tbxFilename.Text.LastIndexOf(@"\") );
+
+
+                btnOutput.Enabled = false;
+            }
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!tbxFilename.Text.Equals(""))
+            {
+                AnalyzeFile(tbxFilename.Text);
+                btnOutput.Enabled = true;
+            }
+        }
+
+        ArrayList fLines = new ArrayList();
+        private void AnalyzeFile(String filename)
+        {
+            this.lblStatus.Text = "分析中...";
+            this.lvPreview.Items.Clear();
+
+
+            StreamReader sr = new StreamReader(filename, Encoding.GetEncoding("GB2312"));
+            String line;
+            int i = 0;
+            fLines = new ArrayList();
+
+            while ((line = sr.ReadLine()) != null)
+            {
+
+                Match m = Regex.Match(line, tbxRegularExpression.Text);
+                if (m.Success)
+                {
+                    ListViewItem lvi = new ListViewItem(line);
+                    lvi.Text = line;
+                    lvi.Checked = true;
+                    lvi.SubItems.Add(i + "");
+                    lvPreview.Items.Add(lvi);
+                }
+                i++;
+                fLines.Add(line);
+            }
+            sr.Close();
+            this.lblStatus.Text = "分析已完成";
+        }
+
+
+        private String zeroleading(int i)
+        {
+            if (i < 10) return "0" + i;
+            else return "" + i;
+        }
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            
+
+            DumpFile();
+            this.lblStatus.Text = "输出已完成";
+        }
+
+        private void DumpFile()
+        {
+            ArrayList lines = new ArrayList();
+            lines.Add(0);
+            for (int i = 0; i < lvPreview.Items.Count; i++)
+            {
+                if (lvPreview.Items[i].Checked)
+                {
+                    int p = int.Parse(lvPreview.Items[i].SubItems[1].Text);
+
+                    if (p!=0)
+                    lines.Add(p);
+                }
+            }
+
+
+            int EveryNChapter = int.Parse(tbxEveryNChapter.Text);
+            if (EveryNChapter > 1)
+            {
+                int j = 0;
+                while (j < lines.Count)
+                {
+                    try
+                    {
+                        for (int k=0;k<EveryNChapter-1;k++)
+                        lines.RemoveAt(j + 1);
+                    }
+                    catch (Exception e) { }
+                    j++;
+                }
+            }
+
+
+            int linesCount = 0;
+            String outputFilename = tbxOutputDirectory.Text + @"\" + string.Format(tbxOutputFilename.Text, zeroleading(1+linesCount));
+            StreamWriter sw = new StreamWriter(outputFilename);
+
+            for (int i = 0; i < fLines.Count; i++)
+            {
+                if (linesCount >= lines.Count) { linesCount = lines.Count - 1; }
+                int CurrentLowerBoundary = int.Parse(lines[linesCount].ToString());
+                if (i == CurrentLowerBoundary)
+                {
+                    sw.Close();
+                    outputFilename = tbxOutputDirectory.Text + @"\" + string.Format(tbxOutputFilename.Text, zeroleading(1+linesCount++));
+                    sw = new StreamWriter(outputFilename);
+                }
+
+                if (checkBox1.Checked && fLines[i].Equals(""))
+                {
+                }
+                else
+                {
+                    sw.WriteLine(fLines[i]);
+                }
+            }
+
+            sw.Close();
+
+
+        }
+    }
+}
